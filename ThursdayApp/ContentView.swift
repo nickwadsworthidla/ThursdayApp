6648003e856b745a0b84e591bfd40469730f0943
Nick Wadsworth <nick.wadsworth@idla.k12.id.us>
2019-11-14 08:46:13 -0700

    Initial Commit

---
+++ ThursdayApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThursdayApp
+//
+//  Created by Nick Wadsworth on 11/14/19.
+//  Copyright © 2019 Nick Wadsworth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
